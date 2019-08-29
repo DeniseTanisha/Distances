658b68d3ee3457e9b83e6a75a073a1663c3c1bd4
Denise Stubbs <denise.stubbs@Nology.local>
2019-08-29 13:33:25 +0100

    front-end started

--- main.js
+++ main.js
@@ -1,5 +1,83 @@
-const sum = (a, b) => {
-  return a + b;
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+// module.exports = sum;
+
+const graph = {
+  A: { C: 2 },
+  B: { D: 4, E: 7 },
+  C: { F: 4, D: 1, A: 2 },
+  D: { B: 4, G: 2, C: 1, F: 1 },
+  E: { B: 7, H: 10 },
+  F: { C: 4, D: 1, G: 3 },
+  G: { D: 2, F: 3, H: 4 },
+  H: { G: 4, E: 10 }
 };
 
-module.exports = sum;
+const distance = (startPoint, endPoint) => {};
+
+console.log(graph.A.C);
+
+// const ShortestDistanceRoute = (point, processed) => {
+//   return Object.keys(point).reduce((lowest, distance) => {
+//     if (lowest === null || point[distance] < point[lowest]) {
+//       if (!processed.includes(distance)) {
+//         lowest = distance;
+//       }
+//     }
+//     return lowest;
+//   }, null);
+// };
+
+// // function that returns the minimum cost and path to reach Finish
+// const routes = distances => {
+//   // track lowest cost to reach each node
+//   const distances = Object.assign({ finish: Infinity }, graph.start);
+
+//   // track paths
+//   const parents = { finish: null };
+//   for (let child in graph.start) {
+//     parents[child] = "start";
+//   }
+
+//   // track nodes that have already been processed
+//   const processed = [];
+
+//   let node = lowestCostNode(costs, processed);
+
+//   while (node) {
+//     let cost = costs[node];
+//     let children = graph[node];
+//     for (let n in children) {
+//       let newCost = cost + children[n];
+//       if (!costs[n]) {
+//         costs[n] = newCost;
+//         parents[n] = node;
+//       }
+//       if (costs[n] > newCost) {
+//         costs[n] = newCost;
+//         parents[n] = node;
+//       }
+//     }
+//     processed.push(node);
+//     node = lowestCostNode(costs, processed);
+//   }
+
+//   let optimalPath = ["finish"];
+//   let parent = parents.finish;
+//   while (parent) {
+//     optimalPath.push(parent);
+//     parent = parents[parent];
+//   }
+//   optimalPath.reverse();
+
+//   const results = {
+//     distance: costs.finish,
+//     path: optimalPath
+//   };
+
+//   return results;
+// };
+
+// console.log(dijkstra(problem));
--- main.test.js
+++ main.test.js
@@ -2,7 +2,7 @@ const distance = require("./main.js");
 
 test("difference between two points to be shortest route possible", () => {
   expect(distance(A, C)).toBe(2);
-  expect(differnce(C, D)).toBe(1);
+  expect(distance(C, D)).toBe(1);
   expect(distance(C, F)).toBe(4);
   expect(distance(B, D)).toBe(4);
   expect(distance(B, E)).toBe(7);
@@ -11,4 +11,8 @@ test("difference between two points to be shortest route possible", () => {
   expect(distance(F, G)).toBe(3);
   expect(distance(G, H)).toBe(4);
   expect(distance(E, H)).toBe(10);
+});
+
+test("the routes should always come up with a distance", () => {
+  expect(distance(A, H)).not.toBe(0);
 });
